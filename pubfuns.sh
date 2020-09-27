3004d9fbddaa4fc79ae85e9c609cb8bd1f0b288b
Timothee Cour <timothee.cour2@gmail.com>
2020-09-26 17:57:48 -0700

    _

--- pubfuns.sh
+++ pubfuns.sh
@@ -1,16 +1,19 @@
 set -u
 
 d_version(){
-  echo "v0.1"
+  echo "v0.2"
 }
 
 d_install_opencv(){
-  cd ~/git_clone/opencv/build/
-  time cmake -D CMAKE_BUILD_TYPE=RELEASE \
-      -D CMAKE_INSTALL_PREFIX=/usr/local \
-      -D INSTALL_C_EXAMPLES=ON \
-      -D INSTALL_PYTHON_EXAMPLES=ON \
-      -D OPENCV_GENERATE_PKGCONFIG=ON \
-      -D OPENCV_EXTRA_MODULES_PATH=~/git_clone/opencv_contrib/modules \
-      -D BUILD_EXAMPLES=ON ..
+  (
+    set -e
+    cd ~/git_clone/opencv/build/
+    time cmake -D CMAKE_BUILD_TYPE=RELEASE \
+        -D CMAKE_INSTALL_PREFIX=/usr/local \
+        -D INSTALL_C_EXAMPLES=ON \
+        -D INSTALL_PYTHON_EXAMPLES=ON \
+        -D OPENCV_GENERATE_PKGCONFIG=ON \
+        -D OPENCV_EXTRA_MODULES_PATH=~/git_clone/opencv_contrib/modules \
+        -D BUILD_EXAMPLES=ON ..
+  )
 }
